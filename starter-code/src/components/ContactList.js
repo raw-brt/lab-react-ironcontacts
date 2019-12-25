025528860238a8aaf24d5b1cd74755b64f66ac7f
raw-brt <robertodzbt@gmail.com>
2019-12-25 11:20:29 +0100

    Random functionality added but button now showing

--- starter-code/src/components/ContactList.js
+++ starter-code/src/components/ContactList.js
@@ -5,9 +5,18 @@ class ContactList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      contacts: this.props.contacts.slice(0, 5) || []
+      contacts: this.props.contacts.slice(0, 5) || [],
+      allContacts: this.props.contacts || []
       }
     }
+
+  onClickRandomContact = () => {
+    const remainingContacts = this.state.allContacts.filter(contact => !this.state.contacts.some(contact => contact === contact));
+    const randomContact = remainingContacts[Math.floor(Math.random() * remainingContacts.length)];
+    if (randomContact) {
+      this.setState({contacts: [...this.state.contacts, randomContact]})
+    }
+  }
 
     render () {
       const Head = () => {
@@ -33,11 +42,22 @@ class ContactList extends Component {
         )
       }
 
+      const Buttons = () => {
+        return (
+          <div className="btn-group mb-4" role="group" aria-label="example">
+            <button type="button" className="btn btn-secondary" onClick={this.onClickRandomContact}>+</button>
+          </div>
+        )
+      }
+
       return (
-        <table className="table">
-          <Head />
-          <Body />
-        </table>
+        <div>
+          <Buttons />
+            <table className="table">
+              <Head />
+              <Body />
+            </table>
+        </div>
       )
     }
   }
